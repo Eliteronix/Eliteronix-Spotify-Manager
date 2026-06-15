b6ec1965a03d2f921e5e26f88655bb24f34e3418
Eliteronix <zimmermann.mariomarvin@gmail.com>
2026-06-15 22:13:18 +0200

    Added removing liked songs from an archive playlist

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ Getting the spotify token with enough rights:
 1. Create client https://developer.spotify.com/dashboard
 
 2. Open this in browser (Make sure the callback uri is added as a valid uri in the app on developers in spotify):
-https://accounts.spotify.com/authorize?client_id=<CLIENT_ID>&response_type=code&redirect_uri=http://127.0.0.1:8888/callback&scope=user-modify-playback-state%20user-read-playback-state%20user-library-read%20user-library-modify
+https://accounts.spotify.com/authorize?client_id=<CLIENT_ID>&response_type=code&redirect_uri=http://127.0.0.1:8888/callback&scope=user-modify-playback-state%20user-read-playback-state%20user-library-read%20user-library-modify%20playlist-modify-public%20playlist-modify-private
 
 3. Copy the code from the result in the URL bar
 
--- index.js
+++ index.js
@@ -3,10 +3,16 @@ require('dotenv').config();
 let spotifyToken = null;
 let spotifySavedTracks = [];
 let duplicateIndexes = [];
+let spotifyArchivePlaylistTracks = [];
+let spotifyArchiveTracksToRemove = [];
+let removedDuplicates = false;
 
 (async () => {
+	await getSpotifyPlaylist(process.env.SPOTIFY_ARCHIVE_PLAYLIST_ID);
+
 	await getSavedSpotifyTracks();
 
+	// eslint-disable-next-line no-console
 	console.log(`Total saved tracks fetched from Spotify: ${spotifySavedTracks.length}`);
 
 	let trackData = [];
@@ -15,8 +21,6 @@ let duplicateIndexes = [];
 		let item = spotifySavedTracks[i];
 
 		trackData.push({ index: i, track: `${item.track.artists.map(artist => artist.name).join(', ')} - ${item.track.name}` });
-
-		// console.log(trackData[i]);
 
 		let duplicateTracks = trackData.filter(t => t.track === trackData[i].track);
 
@@ -30,12 +34,14 @@ let duplicateIndexes = [];
 	}
 
 	if (duplicateIndexes.length > 0) {
+		// eslint-disable-next-line no-console
 		console.log(`Found ${duplicateIndexes.length} duplicate tracks in your saved Spotify tracks:`);
 
 		duplicateIndexes.sort((a, b) => a - b);
 
 		for (let i = 0; i < duplicateIndexes.length; i++) {
 			let item = spotifySavedTracks[duplicateIndexes[i]];
+			// eslint-disable-next-line no-console
 			console.log(`[Index ${duplicateIndexes[i]}] ${item.track.artists.map(artist => artist.name).join(', ')} - ${item.track.name}`);
 
 			if (!spotifyToken) {
@@ -52,15 +58,84 @@ let duplicateIndexes = [];
 				},
 			}).then(async (response) => {
 				if (response.status === 200 || response.status === 204) {
+					// eslint-disable-next-line no-console
 					console.log('   Successfully deleted duplicate track from saved tracks.');
 				} else {
+					// eslint-disable-next-line no-console
 					console.log(`   Failed to delete duplicate track. Status code: ${response.status}`);
 				}
 			});
 		}
-	} else {
-		console.log('No duplicate tracks found in your saved Spotify tracks.');
 
+		removedDuplicates = true;
+	} else {
+		// eslint-disable-next-line no-console
+		console.log('No duplicate tracks found in your saved Spotify tracks.');
+	}
+
+	for (let i = 0; i < spotifyArchivePlaylistTracks.length; i++) {
+		let item = spotifyArchivePlaylistTracks[i];
+
+		let trackInfo = `${item.track.artists.map(artist => artist.name).join(', ')} - ${item.track.name}`;
+
+		let savedTrack = trackData.filter(t => t.track === trackInfo);
+
+		if (savedTrack.length > 0) {
+			spotifyArchiveTracksToRemove.push(item);
+		}
+	}
+
+	if (spotifyArchiveTracksToRemove.length > 0) {
+		// eslint-disable-next-line no-console
+		console.log(`Found ${spotifyArchiveTracksToRemove.length} already liked tracks in your archive playlist:`);
+
+		spotifyArchiveTracksToRemove.sort((a, b) => a - b);
+
+		for (let i = 0; i < spotifyArchiveTracksToRemove.length; i++) {
+			let item = spotifyArchiveTracksToRemove[i];
+			// eslint-disable-next-line no-console
+			console.log(`[Index ${i}] ${item.track.artists.map(artist => artist.name).join(', ')} - ${item.track.name}`);
+
+			if (!spotifyToken) {
+				await getSpotifyToken();
+			}
+
+			// Delete the duplicate track from archive playlist
+			const deleteUrl = `https://api.spotify.com/v1/playlists/${process.env.SPOTIFY_ARCHIVE_PLAYLIST_ID}/items`;
+
+			let body = JSON.stringify({
+				'items': [
+					{
+						'uri': item.track.uri
+					}
+				]
+			});
+
+			await fetch(deleteUrl, {
+				method: 'DELETE',
+				headers: {
+					'Authorization': `Bearer ${spotifyToken}`,
+					'Content-Type': 'application/json',
+				},
+				body: body,
+			}).then(async (response) => {
+				if (response.status === 200 || response.status === 204) {
+					// eslint-disable-next-line no-console
+					console.log('   Successfully deleted liked track from archive playlist.');
+				} else {
+					// eslint-disable-next-line no-console
+					console.log(`   Failed to delete liked track. Status code: ${response.status}`);
+				}
+			});
+		}
+
+		removedDuplicates = true;
+	} else {
+		// eslint-disable-next-line no-console
+		console.log('No already liked tracks found in your archive playlist.');
+	}
+
+	if (!removedDuplicates) {
 		// Kill the process if no duplicates are found after 5 seconds
 		setTimeout(() => {
 			process.exit(0);
@@ -89,10 +164,41 @@ async function getSavedSpotifyTracks(offset = 0, limit = 50) {
 
 		spotifySavedTracks = spotifySavedTracks.concat(json.items);
 
+		// eslint-disable-next-line no-console
 		console.log(`Fetched ${spotifySavedTracks.length} saved tracks from Spotify so far.`);
 
 		if (json.total > limit + offset) {
 			await getSavedSpotifyTracks(offset + limit, limit);
+		}
+	});
+}
+
+async function getSpotifyPlaylist(playlistId, offset = 0, limit = 50) {
+	if (!spotifyToken) {
+		await getSpotifyToken();
+	}
+
+	const searchParams = new URLSearchParams({
+		limit: limit.toString(),
+		offset: offset.toString(),
+	});
+
+	const searchUrl = `https://api.spotify.com/v1/playlists/${playlistId}/items?${searchParams.toString()}`;
+
+	await fetch(searchUrl, {
+		headers: {
+			'Authorization': `Bearer ${spotifyToken}`,
+		},
+	}).then(async (response) => {
+		let json = await response.json();
+
+		spotifyArchivePlaylistTracks = spotifyArchivePlaylistTracks.concat(json.items);
+
+		// eslint-disable-next-line no-console
+		console.log(`Fetched ${spotifyArchivePlaylistTracks.length} tracks from the archive playlist so far.`);
+
+		if (json.total > limit + offset) {
+			await getSpotifyPlaylist(playlistId, offset + limit, limit);
 		}
 	});
 }
